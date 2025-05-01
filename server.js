3b12d378816bbe823812f985d03b211bb58f3ee2
Lata Chapadia <latarana4u@gmail.com>
2025-05-01 12:42:56 +0530

    initial backend changed

--- server.js
+++ server.js
@@ -13,6 +13,7 @@ console.log('MongoDB URI:', process.env.MONGODB_URI);
 mongoose.connect(process.env.MONGODB_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
+  serverSelectionTimeoutMS: 30000, // Increase timeout to 30 seconds
 })
 .then(() => {
   console.log('Connected to MongoDB');
